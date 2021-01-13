51928935ea161d67a7d9d64967615e2c7e1e336c
zhuhaoxin666 <1302356967@qq.com>
2021-01-14 00:57:36 +0800

    springboot-hello world创建完成！

---
+++ springboot-helloworld/src/main/java/com/example/springboothelloworld/myfunction/Myfunction.java
@@ -0,0 +1,11 @@
+package com.example.springboothelloworld.myfunction;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Myfunction {
+    public int add(int x,int y){
+        return x+y;
+    }
+
+}
